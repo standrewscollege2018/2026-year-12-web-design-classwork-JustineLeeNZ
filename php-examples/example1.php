b8dd483f44cff539dd300ee691cd3520c8c3a8c1
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2026-03-09 19:11:38 +1300

    PHP basics - how to use PHP tags, variable, echo string & variables

---
+++ php-examples/example1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Very basic PHP page</title>
+</head>
+<body>
+
+<h1>This is my first PHP page</h1>
+
+<?php
+    $teacher_name = "Mr Van Flo";
+
+    echo "My favourite teacher is...";
+    echo $teacher_name;
+
+?>    
+    
+</body>
+</html>
