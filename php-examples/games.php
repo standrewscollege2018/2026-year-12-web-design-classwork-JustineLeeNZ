b6c3a94c39d6aeb4e7f9e4eb82151f6e15c41640
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2026-03-16 12:12:13 +1300

    Demo of using GET array to send info between pages
    List of genres as clickable links (genres_list.php), genre id passed on GET array then used in query in next page (games.php) so that only games of selected genre are displayed

---
+++ php-examples/games.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>Games in selected genre</H1>
+
+
+    <!-- connect to database -->
+    <?php include("db_connect.php"); 
+
+    // get info from GET array
+    $genre = $_GET['genre_id'];
+
+    // make and run query
+    // query will return only ONE record i.e. Hollow Knight
+    $game_sql = "SELECT * FROM game WHERE genre_id=" . $genre;
+    $game_qry = mysqli_query($db_connect, $game_sql);
+    $game_aa = mysqli_fetch_assoc($game_qry);
+
+
+
+    // display results
+    do {
+        echo "<p><em>Name:</em> " . $game_aa['game_name'] . "  <em>Released:</em> " . $game_aa['release_year'];
+
+
+        } while ($game_aa = mysqli_fetch_assoc($game_qry))
+
+
+   ?> 
+
+</body>
+</html>
---
+++ php-examples/genre_list.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=p, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Game genres</h1>
+
+    <p><a href="games.php?genre_id=1">Action</a></p>
+    <p><a href="games.php?genre_id=3">Adventure</a></p>
+    <p><a href="games.php?genre_id=9">Platformer</a></p>
+    <p><a href="games.php?genre_id=2">Puzzle</a></p>
+
+</body>
+</html>
